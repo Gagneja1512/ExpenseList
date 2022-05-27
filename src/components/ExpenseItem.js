26265363c33d917c0782cb5047428909c0e496ec
gagneja1512 <aadityaarora1215@gmail.com>
2022-05-27 16:44:01 +0530

    Adding Basic JS expressions

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+    const expenseDate = new Date(2022 , 4 , 27) ;
+    const expenseTitle = 'Car Insurance' ;
+    const expenseAmount = 3499 ;
+
   return (
     <div className="expense-item">
-      <div>March 28th 2022</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">Rs.3499</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">Rs.{expenseAmount}</div>
       </div>
     </div>
   );
