01a84ebcc55de05c286b69d75edba4d478556e57
gagneja1512 <aadityaarora1215@gmail.com>
2022-05-30 14:04:28 +0530

    Adding year filter

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,25 +1,40 @@
-import React , {useState} from "react";
+import React, { useState } from "react";
 
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css'
 import ExpensesFilter from "./ExpensesFilter";
 
-function Expenses(props){
-    const [filteredYear , setFilteredYear] = useState('2022') ;
+function Expenses(props) {
+    const [filteredYear, setFilteredYear] = useState('2022');
 
-    const filterChangeHandler = (selectedYear) =>{
+    const filterChangeHandler = (selectedYear) => {
         //console.log('Expensesjs') ;
         //console.log(selectedYear) ;
-        setFilteredYear(selectedYear) ;
+        setFilteredYear(selectedYear);
     };
+
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
+    let expensesContent = <p>No Expenses found</p> ;
+    if(filteredExpenses.length > 0)
+    {
+        expensesContent = filteredExpenses.map((expense) => 
+        (
+            <ExpenseItem key={expense.id} 
+            title={expense.title} 
+            amount={expense.amount} 
+            date={expense.date} />
+        )) ;
+    }
 
     return (
         <div>
             <div className="expenses">
-                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
 
-                {props.items.map((expense) => <ExpenseItem title ={expense.title} amount = {expense.amount} date={expense.date} />)}
-
+                {expensesContent}
                 {/*<ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
                 <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItem>
                 <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}></ExpenseItem>
@@ -29,4 +44,4 @@ function Expenses(props){
     )
 }
 
-export default Expenses ;
+export default Expenses;
