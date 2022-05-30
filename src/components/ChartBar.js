b562c782047b74f97004692166876cdd37a2dd88
gagneja1512 <aadityaarora1215@gmail.com>
2022-05-31 01:53:24 +0530

    Wrapping up the Expense Projet

--- src/components/ChartBar.js
+++ src/components/ChartBar.js
@@ -5,7 +5,7 @@ import './ChartBar.css'
 const ChartBar = (props) => {
 
     let barFillHeight = '0%'
-    if(props.max > 0)
+    if(props.maxValue > 0)
     {
         barFillHeight = Math.round((props.value/props.maxValue)*100) + '%';
     }    
@@ -13,7 +13,7 @@ const ChartBar = (props) => {
     return (
         <div className="chart-bar">
             <div className="chart-bar__inner">
-                <div className="chart-bar__fill" style={{height : barFillHeight}}> </div>
+                <div className="chart-bar__fill" style={{height : barFillHeight}}></div>
             </div>
             <div className="chart-bar__label">{props.label}</div>
         </div>
